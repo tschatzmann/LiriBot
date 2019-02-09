2340f02e65110a44eea3a93fc92e6293b791c37b
tschatzmann <tarrta2002@gmail.com>
2019-02-09 16:52:13 -0500

    updated urls

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+let axios = require("axios")
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 
@@ -6,58 +7,72 @@ var spotify = new Spotify(keys.spotify);
 
 
 cmd = process.argv[2];
-searchValue = process.argv[3];
+searchValue = process.argv.slice(3).join(' ');
+console.log(searchValue)
+console.log('the command ' + cmd);
 
-getCmd(cmd)
 
-console.log(keys);
-console.log(cmd)
-console.log(spotify);
 
-var bandCmd = 'concert-this'
 
-var spotifyCmd = 'spotify-this-song'
+var bandCmd = 'concert-this';
 
-var movieCmd = 'movie-this'
+var spotifyCmd = 'spotify-this-song';
 
-var doitCmd = 'do-what-it-says'
+var movieCmd = 'movie-this';
+
+var doitCmd = 'do-what-it-says';
 
     var omdbApi = keys.omdbApiKey.id;
-    console.log(omdbApi)
-
+   // console.log(omdbApi)
+var bandsURL = `https://rest.bandsintown.com/artists/${searchValue}/events?app_id=codingbootcamp`
 var movieURL = `http://www.omdbapi.com/?t=${searchValue}&y=&plot=short&apikey=${omdbApi}`
-var bandsURL = `https://rest.bandsintown.com/artists/"${searchValue}"/events?app_id=codingbootcamp`
+//var bandsURL = `https://rest.bandsintown.com/artists/"${searchValue}"/events?app_id=codingbootcamp`
 console.log(bandsURL);
 console.log(movieURL);
-
+getCmd(cmd)
 function getCmd(cmd){
         switch (cmd){
         case bandCmd:
             console.log('this is band');
             console.log(searchValue)
+            getBands();
             break;
         case spotifyCmd:
             console.log('this is an actor');
             console.log(searchValue)
+          getSpotifySong(searchValue)
             break;
        case movieCmd:
         console.log('move')
         console.log(searchValue)
+        getMovies(searchValue);
         break;
        case doitCmd:
        console.log('doit')
+       break;
     }
 }
 
-getSpotifySong('Boom')
 
-function getSpotifySong(){
+
+
+function getSpotifySong(song){
+   console.log(song);
     spotify
-  .search({ type: 'text', query: `${searchValue}` })
+  .search({ type: 'track', query: `${song}` })
   .then(function(response) {
     console.log(response);
   })
   .catch(function(err) {
     console.log(err);
   });
+}
+console.log('sent bands ' + searchValue);
+
+function getBands(){
+    console.log(bandsURL)
+    axios.get(bandsURL).then(function(response){
+        console.log(response.data);
+    })
+
 }
